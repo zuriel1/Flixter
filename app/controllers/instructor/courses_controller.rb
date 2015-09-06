4d6132f4fef25a35951454b9784a6fa5759023a4
vagrant <zuriel.wz@gmail.com>
2015-09-06 17:43:07 +0000

    Validations to courses

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -6,9 +6,13 @@ class Instructor::CoursesController < ApplicationController
   	@course = Course.new
   end
   
-  def create
+   def create
     @course = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
 
   def show
