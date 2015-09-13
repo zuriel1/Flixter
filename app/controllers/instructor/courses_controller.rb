792b17707d3a7e9ca9ebb371fa78dbb3346f86cc
vagrant <zuriel.wz@gmail.com>
2015-09-13 01:28:10 +0000

    Permission

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::CoursesController < ApplicationController
-  
   before_action :authenticate_user!
+  
+  before_action :require_authorized_for_current_course, :only => [:show]
 
   def new
     @course = Course.new
@@ -8,7 +9,6 @@ class Instructor::CoursesController < ApplicationController
 
   def create
     @course = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
     if @course.valid?
       redirect_to instructor_course_path(@course)
     else
@@ -17,10 +17,19 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render :text => "Unauthorized", :status => :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
